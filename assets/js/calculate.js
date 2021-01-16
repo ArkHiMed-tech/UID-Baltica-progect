b7816d3de8860c0f5e2457d942500375612307a9
ArkHiMed-tech <74063313+ArkHiMed-tech@users.noreply.github.com>
2021-01-16 16:04:15 +0200

    Обновление скриптов

--- assets/js/calculate.js
+++ assets/js/calculate.js
@@ -1,11 +1,24 @@
 var speed = document.querySelector('#calculate');
-var output = document.querySelector('#calculate-output');
-var distance;
+var outputDry = document.querySelector('#calculate-output-dry');
+var outputWet = document.querySelector('#calculate-output-wet');
+var outputSnow = document.querySelector('#calculate-output-snow');
+var outputIce = document.querySelector('#calculate-output-ice');
+
+var distanceDry;
+var distanceWet;
+var distanceSnow;
+var distanceIce;
 
 speed.addEventListener('input', function (evt) {
     evt.preventDefault();
     var speedValue = speed.value;
-    distance = (speedValue / 10) * (speedValue / 10);
-    distance = distance.toFixed(2);
-    output.value = distance;
+    distanceDry = (speedValue * speedValue / (254 * 0.7)).toFixed(2);
+    distanceWet = (speedValue * speedValue / (254 * 0.4)).toFixed(2);
+    distanceSnow = (speedValue * speedValue / (254 * 0.2)).toFixed(2);
+    distanceIce = (speedValue * speedValue / (254 * 0.1)).toFixed(2);
+
+    outputDry.value = distanceDry;
+    outputWet.value = distanceWet;
+    outputSnow.value = distanceSnow;
+    outputIce.value = distanceIce;
 });
